3cbc98e531bb44f5fe8a1d0b4d2d679a457f0f63
finalmegaman NA <finalmegaman@gmail.com>
2019-03-03 13:17:32 -0800

    [32 Creating Dialog Manager & Reading Text]

---
+++ Assets/Scripts/DIalogManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DIalogManager : MonoBehaviour {
+
+	public Text dialogText;
+	public Text nameText;
+	public GameObject dialogBox;
+	public GameObject nameBox;
+
+	public string[] dialogLines;
+
+	public int currentLine;
+
+	// Use this for initialization
+	void Start () {
+		dialogText.text = dialogLines[0];
+		nameText.text = "Gandalf";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
