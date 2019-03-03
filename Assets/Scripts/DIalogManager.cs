ecd954329d0e220ac17d1a533537ac5b9f6f43d0
finalmegaman NA <finalmegaman@gmail.com>
2019-03-03 13:46:06 -0800

    [34 Activating & switching lines]

--- Assets/Scripts/DIalogManager.cs
+++ Assets/Scripts/DIalogManager.cs
@@ -14,24 +14,41 @@ public class DialogManager : MonoBehaviour {
 
 	public int currentLine;
 
+	bool justStarted;
+
+	public static DialogManager instance;
+
 	// Use this for initialization
 	void Start () {
-		dialogText.text = dialogLines[currentLine];
-		nameText.text = "Gandalf";
+		instance = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (dialogBox.activeInHierarchy) {
 			if (Input.GetButtonUp("Fire1")) {
-				currentLine++;
+				if (!justStarted) {
+					currentLine++;
 
-				if (currentLine >= dialogLines.Length) {
-					dialogBox.SetActive(false);
-				} else {
-					dialogText.text = dialogLines[currentLine];
+					if (currentLine >= dialogLines.Length) {
+						dialogBox.SetActive(false);
+					} else {
+						dialogText.text = dialogLines[currentLine];
+					}
 				}
+
+				justStarted = false;
 			}
 		}
+	}
+
+	public void ShowDialog(string[] newLines){
+		dialogLines = newLines;
+		currentLine = 0;
+
+		dialogText.text = dialogLines[currentLine];
+
+		dialogBox.SetActive(true);
+		justStarted = true;
 	}
 }
