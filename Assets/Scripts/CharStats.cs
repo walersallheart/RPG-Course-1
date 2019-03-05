853e3f017a70326167886c4d70c5ed64776931fc
finalmegaman NA <finalmegaman@gmail.com>
2019-03-04 16:47:56 -0800

    38 Player Stats

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour {
+
+	public string charName;
+	public string charClass;
+	public int playerLevel=1;
+	public int currentEXP = 0;
+
+	public int currentHP;
+	public int maxHP=100;
+	public int currentMP;
+	public int maxMP=30;
+	public int strength;
+	public int defense;
+	public int wpnPwr;
+	public int armrPwr;
+	public string equippedWpn;
+	public string equippedArmr;
+	public Sprite charImage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
