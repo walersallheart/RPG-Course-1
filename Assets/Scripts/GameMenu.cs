a86995efc2058bc5a4a57f7d11818888c4e2e55b
finalmegaman NA <finalmegaman@gmail.com>
2019-03-05 17:46:57 -0800

    47 Setting up Stat references

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour {
 
 	public GameObject theMenu;
+
+	private CharStats[] playerStats;
+
+	public Text[] nameText, hpText, mpText, lvlText, expText;
+	public Slider[] expSlider;
+	public Image[] charImage;
+	public GameObject[] charStatHolder;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +27,20 @@ public class GameMenu : MonoBehaviour {
 				GameManager.instance.gameMenuOpen = false;
 			} else {
 				theMenu.SetActive(true);
+				UpdateMainStats();
 				GameManager.instance.gameMenuOpen = true;
+			}
+		}
+	}
+
+	public void UpdateMainStats(){
+		playerStats = GameManager.instance.playerStats;
+
+		for(int i = 0; i<playerStats.Length; i++){
+			if (playerStats[i].gameObject.activeInHierarchy) {
+				charStatHolder[i].SetActive(true);
+			} else {
+				charStatHolder[i].SetActive(false);
 			}
 		}
 	}
