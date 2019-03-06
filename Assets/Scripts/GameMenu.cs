7940494047e75ce8d6d29c4d10f38ad767ad1521
finalmegaman NA <finalmegaman@gmail.com>
2019-03-05 17:20:55 -0800

    45 Opening & Closing the menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMenu : MonoBehaviour {
+
+	public GameObject theMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire2") || Input.GetKeyDown(KeyCode.M)) {
+			if (theMenu.activeInHierarchy) {
+				theMenu.SetActive(false);
+			} else {
+				theMenu.SetActive(true);
+			}
+		}
+	}
+}
