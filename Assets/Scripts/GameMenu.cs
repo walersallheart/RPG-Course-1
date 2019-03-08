820b33b51a4a87e24aa826f759ef6dd662c09c9d
finalmegaman NA <finalmegaman@gmail.com>
2019-03-07 18:06:14 -0800

    60 Using Discard Button

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -146,4 +146,10 @@ public class GameMenu : MonoBehaviour {
 		itemName.text = activeItem.itemName;
 		itemDescription.text = activeItem.description;
 	}
+
+	public void DiscardItem(){
+		if (activeItem != null) {
+			GameManager.instance.RemoveItem(activeItem.itemName);
+		}
+	}
 }
