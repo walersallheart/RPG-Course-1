b11443337134721f03120724ec1919b2c98d19d5
finalmegaman NA <finalmegaman@gmail.com>
2019-03-07 17:09:18 -0800

    56 Showing the Items in menu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -7,17 +7,15 @@ public class GameMenu : MonoBehaviour {
 
 	public GameObject theMenu;
 	public GameObject[] windows;
-
 	private CharStats[] playerStats;
-
 	public Text[] nameText, hpText, mpText, lvlText, expText;
 	public Slider[] expSlider;
 	public Image[] charImage;
 	public GameObject[] charStatHolder;
 	public GameObject[] statusButtons;
-
 	public Text statusName, statusClass, statusHP, statusMP, statusStr, statusDef, statusWpnEqpd, statusWpnPwr, statusArmrEqpd, statusArmrPwr, statusExp;
 	public Image statusImage;
+	public ItemButton[] itemButtons;
 
 	// Use this for initialization
 	void Start () {
@@ -113,5 +111,20 @@ public class GameMenu : MonoBehaviour {
 
 		statusExp.text = (playerStats[selected].expToNextLevel[playerStats[selected].playerLevel] - playerStats[selected].currentEXP).ToString();
 		statusImage.sprite = playerStats[selected].charImage;
+	}
+
+	public void ShowItems(){
+		for (int i = 0; i<itemButtons.Length; i++){
+			itemButtons[i].buttonValue = i;
+
+			if(GameManager.instance.itemsHeld[i] != "") {
+				itemButtons[i].buttonImage.gameObject.SetActive(true);
+				itemButtons[i].buttonImage.sprite = GameManager.instance.GetItemDetails(GameManager.instance.itemsHeld[i]).itemSprite;
+				itemButtons[i].amountText.text = GameManager.instance.numberOfItems[i].ToString();
+			} else {
+				itemButtons[i].buttonImage.gameObject.SetActive(false);
+				itemButtons[i].amountText.text = "";
+			}
+		}
 	}
 }
