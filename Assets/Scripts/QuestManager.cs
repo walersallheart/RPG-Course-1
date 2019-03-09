12be6e83220d17548dc13293e140efde4db739fd
finalmegaman NA <finalmegaman@gmail.com>
2019-03-09 11:32:19 -0800

    71 Making Quests

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+	public string[] questMarkerNames;
+	public bool[] questMarkersComplete;
+
+	public static QuestManager instance;
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+
+		questMarkersComplete = new bool[questMarkerNames.Length];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
