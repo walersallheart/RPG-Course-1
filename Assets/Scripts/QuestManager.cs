f7f8a08847677bcc9d889dcf51c7f4a7545dc6a0
finalmegaman NA <finalmegaman@gmail.com>
2019-03-09 11:44:00 -0800

    72 Checking & Marking Quests

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -18,6 +18,35 @@ public class QuestManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			Debug.Log(CheckIfComplete("quest test"));
+		}
+	}
+
+	public int GetQuestNumber(string questToFind) {
+		for (int i = 0; i<questMarkerNames.Length; i++){
+			if (questMarkerNames[i] == questToFind) {
+				return i;
+			}
+		}
+
+		Debug.LogError("Quest " + questToFind + " doest not exist");
+		return 0;
+	}
+
+	public bool CheckIfComplete(string questToCheck) {
+		if (GetQuestNumber(questToCheck) != 0) {
+			return questMarkersComplete[GetQuestNumber(questToCheck)];
+		}
+
+		return false;
+	}
+
+	public void MarkQuestComplete(string questToMark){
+		questMarkersComplete[GetQuestNumber(questToMark)] = true;
+	}
+
+	public void MarkQuestIncomplete(string questToMark) {
+		questMarkersComplete[GetQuestNumber(questToMark)] = false;
 	}
 }
