ac169933175a8c4ce23351f4176430634b527a18
finalmegaman NA <finalmegaman@gmail.com>
2019-03-11 16:48:15 -0700

    88 Creating Battle Characters

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour {
+
+	public bool isPlayer;
+	public string[] movesAvailable;
+
+	public string charName;
+
+	public int currentHP, maxHP, currentMP, maxMP, strength, defense, wpnPower, armrPower;
+	public bool hasDied;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
