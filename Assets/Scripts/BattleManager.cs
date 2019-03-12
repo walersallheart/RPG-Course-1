1003d448d03803cbe1b5faf7eab899fdac670903
finalmegaman NA <finalmegaman@gmail.com>
2019-03-11 17:54:22 -0700

    91 Making Turns

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -16,6 +16,11 @@ public class BattleManager : MonoBehaviour {
 
 	public List<BattleChar> activeBattlers = new List<BattleChar>();
 
+	public int currentTurn;
+	public bool turnWaiting;
+
+	public GameObject uiButtonsHolder;
+
 	// Use this for initialization
 	void Start () {
 		instance = this;
@@ -26,6 +31,22 @@ public class BattleManager : MonoBehaviour {
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.T)) {
 			BattleStart(new string[] {"Eyeball", "Spider", "Skeleton"});
+		}
+
+		if (battleActive) {
+			if (turnWaiting) {
+				if (activeBattlers[currentTurn].isPlayer) {
+					uiButtonsHolder.SetActive(true);
+				} else {
+					uiButtonsHolder.SetActive(false);
+
+					//enemy should attack
+				}
+			}
+
+			if (Input.GetKeyDown(KeyCode.N)) {
+				NextTurn();
+			}
 		}
 	}
 
@@ -74,6 +95,54 @@ public class BattleManager : MonoBehaviour {
 					}
 				}
 			}
+
+			turnWaiting = true;
+			currentTurn = 0;
+		}
+	}
+
+	public void NextTurn(){
+		currentTurn++;
+		
+		if (currentTurn >= activeBattlers.Count) {
+			currentTurn = 0;
+		}
+
+		turnWaiting = true;
+
+		UpdateBattle();
+	}
+
+	public void UpdateBattle(){
+		bool allEnemiesDead = true;
+		bool allPlayersDead = true;
+
+		for (int i = 0; i<activeBattlers.Count; i++){
+			if (activeBattlers[i].currentHP < 0) {
+				activeBattlers[i].currentHP = 0;
+			}
+
+			if (activeBattlers[i].currentHP == 0) {
+				
+			} else {
+				if (activeBattlers[i].isPlayer) {
+					allPlayersDead = false;
+				} else {
+					allEnemiesDead = false;
+				}
+			}
+		}
+
+		if (allEnemiesDead || allPlayersDead) {
+			if (allEnemiesDead) {
+				//end battle in victory
+			} else {
+				//end battle in failure
+			}
+
+			battleScene.SetActive(false);
+			GameManager.instance.battleActive = false;
+			battleActive = false;
 		}
 	}
 }
